8f09d03b95ada538fe13dff5e1f54c7ac1737ffc
Neha Maity <nehamaity@gmail.com>
2025-05-11 13:52:58 -0400

    Solution for revised longest prefix of at least 2 strings in list May 5 2025

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -1,6 +1,9 @@
 # Write a function that takes a list of strings and returns the longest string 
 # that is a prefix of at least two (or all) strings in the list.
 def longest_common_prefix(words):
+    if len(words) == 1:
+        return words[0]
+
     substr = ''
     longest_substr = ''
     for i in range(len(words)):
@@ -9,7 +12,13 @@ def longest_common_prefix(words):
                 if words[j][:s] == words[i][:s]:
                     substr = words[i][:s]
 
+            # Solution for longest substring that is prefix of all items in list
+            '''
             if substr in words[i - 1]:
+                longest_substr = substr
+            '''
+            # Solution for longest string that is prefix of at least two strings in list
+            if substr > longest_substr:
                 longest_substr = substr
 
     return longest_substr
@@ -20,8 +29,8 @@ print(longest_common_prefix(["flower","flow","flight"]))
 print(longest_common_prefix(["dog","racecar","car"]))
 # ""
 
-print(longest_common_prefix(["interstellar","internet","internal","interval"]))
+print(longest_common_prefix(["interstellar","internet","interval"]))
 # "inter"
 
 print(longest_common_prefix(["carton","car","cars","cart"]))
-# "car"
+# "cart"
